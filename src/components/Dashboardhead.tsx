24ba3699b5a1057df6e4c19148e3322d9231545e
OmChillure <omchillure4@gmail.com>
2024-09-29 13:18:43 +0530

    added toggle function

--- src/components/Dashboardhead.tsx
+++ src/components/Dashboardhead.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Link from "next/link";
-import { Button } from "./ui/button";
 import { ModeToggle } from "./mode-toggle";
 import { getCurrentUser, logout } from "@/actions";
 import { useState, useEffect } from "react";
@@ -14,13 +13,15 @@ const DashboardHeader = () => {
   }, []);
 
   return (
-    <nav className="sticky top-0 h-[10vh] flex justify-center items-center z-50 border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <nav className="sticky top-0 h-[10vh] flex justify-center  items-center z-50 border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex justify-between max-h-[10vh] items-center">
         <Link href="/">
-          <h1 className="text-lg mr-4 font-bold sm:inline-block">safeHerspace</h1>
-
+          <h1 className="text-lg font-bold sm:inline-block">safeHerspace</h1>
         </Link>
+        <div className="flex gap-4">
         <Options />
+        <ModeToggle />
+        </div>
       </div>
     </nav>
   );
